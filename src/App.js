3425eac6e3bdde02cc5b50350a66780ea1887f62
carlosqdev <carlosquintero95nic@gmail.com>
2021-08-20 21:47:03 -0600

    Rutas de react-router configuradas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-import Nav from './Nav/Nav';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Nav from "./Nav/Nav";
+import Error404 from "./Error404/Error404";
 // import axios from "axios";
 
 // let apiEndPoint = "https://api.spacexdata.com/v4/launches/latest";
@@ -13,10 +15,29 @@ import Nav from './Nav/Nav';
 //   }
 // };
 
+const Latest = () => {
+  return (
+    <h1>Ultimo lanzamiento</h1>
+  )
+}
+
+const All = () => {
+  return (
+    <h1>Todos los lanzamiento</h1>
+  )
+}
+
 function App() {
   return (
     <>
-      <Nav />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Nav />} />
+          <Route path="/ultimo" element={<Latest />} />
+          <Route path="/todos" element={<All />} />
+          <Route path="*" element={<Error404 />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/Error404/Error404.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error404 = () => {
+  return (
+    <h1>Página no encontrada</h1>
+  )
+}
+
+export default Error404;
