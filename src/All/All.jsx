cddb8756e9d77fb8def1106af26f43be4e0bfeec
carlosqdev <carlosquintero95nic@gmail.com>
2021-08-20 22:19:20 -0600

    Refactor en CSS

--- src/All/All.jsx
+++ src/All/All.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Nav from "../Nav/Nav";
 
 const All = () => {
-  return <h1>Todos los lanzamiento</h1>;
+  return (
+    <>
+      <Nav />
+      <h1>Todos los lanzamiento</h1>
+    </>
+  )
 };
 
 export default All;
