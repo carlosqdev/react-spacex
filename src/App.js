f71afb18fa5aa997b95f0cd307b27b453c9049da
carlosqdev <carlosquintero95nic@gmail.com>
2021-08-21 23:56:10 -0600

    Estructura jsx lista  del componente Latest.jsx

--- src/App.js
+++ src/App.js
@@ -2,20 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Nav/Nav";
 import Error404 from "./Error404/Error404";
-import Latest from './Latest/Latest';
-import All from './All/All';
-// import axios from "axios";
-
-// let apiEndPoint = "https://api.spacexdata.com/v4/launches/latest";
-
-// const getSpacex = async (url) => {
-//   try {
-//     let response = await axios.get(`url`);
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
+import Latest from "./Latest/Latest";
+import All from "./All/All";
 
 const Home = () => {
   return (
@@ -23,9 +11,8 @@ const Home = () => {
       <Nav />
       <h1>Home</h1>
     </>
-  )
-}
-
+  );
+};
 
 function App() {
   return (
@@ -33,7 +20,10 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-          <Route path={`${process.env.PUBLIC_URL}/ultimo`} element={<Latest />} />
+          <Route
+            path={`${process.env.PUBLIC_URL}/ultimo`}
+            element={<Latest />}
+          />
           <Route path={`${process.env.PUBLIC_URL}/todos`} element={<All />} />
           <Route path={`${process.env.PUBLIC_URL}*`} element={<Error404 />} />
         </Routes>
--- src/Latest/Latest.jsx
+++ src/Latest/Latest.jsx
@@ -5,10 +5,7 @@ import axios from "axios";
 const Latest = () => {
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
-  // const [id, setId ] = useState("");
   const [details, setDetails] = useState("");
-  const [webcast, setWebcast] = useState("");
-  const [patch, setPatch] = useState("");
   const [flickr, setFlickr] = useState([]);
 
   useEffect(() => {
@@ -20,10 +17,7 @@ const Latest = () => {
         data.map((item) => ar.push(item));
         setName(response.data.name);
         setDate(response.data.date_local);
-        // setId(response.data.id);
         setDetails(response.data.details);
-        setWebcast(response.data.links.webcast);
-        setPatch(response.data.links.patch.small);
       });
     setFlickr(ar);
   }, []);
@@ -37,26 +31,23 @@ const Latest = () => {
           <h2>{name}</h2>
           <time dateTime="2011-11-18T14:54:39.929">{date}</time>
         </header>
-        <details>
-          <summary>Detalles de la mision</summary>
-          <p>{details}</p>
-        </details>
         <section>
-          {
-            flickr.map((img, index) => (
-              <figure key={index}>
-                <img loading="lazy" src={img} alt={name} width="100" height="100"/>
-                <figcaption>{name}</figcaption>
-              </figure>
-            ))
-          }
+          <p>{details}</p>
         </section>
-        <details>
-          <summary>Video del lanzamiento</summary>
-          <video src={webcast}>
-            Tu navegador no admite el elemento <code>video</code>.
-          </video>
-        </details>
+        <section>
+          {flickr.map((img, index) => (
+            <figure key={index}>
+              <img
+                loading="lazy"
+                src={img}
+                alt={name}
+                width="100"
+                height="100"
+              />
+              <figcaption>{name}</figcaption>
+            </figure>
+          ))}
+        </section>
       </section>
     </>
   );
