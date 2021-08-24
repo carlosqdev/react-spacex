2ff0386e13cd6acce062d2eb5204f34d3660d239
carlosqdev <carlosquintero95nic@gmail.com>
2021-08-23 18:49:57 -0600

    Menu hamburgues

--- src/Nav/Nav.jsx
+++ src/Nav/Nav.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../img/Logo.svg";
+import menu from '../img/menu.svg';
 import "./Nav.css";
 
 function Nav() {
   return (
     <nav className="nav">
-      <img loading="lazy" className="nav__img" src={logo} alt="SPACEX" />
+      <img loading="lazy" className="nav__logo" src={logo} alt="SPACEX" />
+      <label className="nav__label" htmlFor="menu">
+        <img src={menu} alt="menu" />
+      </label>
+      <input type="checkbox" id="menu" className="nav__input" />
       <ul className="nav__menu">
         <li className="nav__item">
           <Link className="nav__link" to={`${process.env.PUBLIC_URL}/`}>Inicio</Link>
