e2a2d0aea5cc94a7e24efcd0b6f398884788427b
carlosqdev <carlosquintero95nic@gmail.com>
2021-08-25 22:16:54 -0600

    Content in Home.jsx with styles

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,10 +1,38 @@
 import Nav from "../Nav/Nav";
+import image from "../img/hls.webp";
+import styled from "styled-components";
+import device from "../breakpoints";
+
+const Section = styled.section`
+  margin: 0 auto;
+  max-width: 2000px;
+  width: 100%;
+  height: auto;
+`;
+const Title = styled.h1`
+  margin: 1em;
+  font-weight: 700;
+  font-size: 1.5em;
+  text-align: center;
+`;
+const Img = styled.img`
+  width: 100%;
+  height: 50vh;
+  margin: auto;
+
+  @media ${device.tablet} {
+    height: 76.8vh;
+  }
+`;
 
 const Home = () => {
   return (
     <>
       <Nav />
-      <h1>Home</h1>
+      <Title>React.js application consuming SPACEX API</Title>
+      <Section>
+        <Img src={image} alt="SpaceX" />
+      </Section>
     </>
   );
 };
---
+++ src/breakpoints.js
@@ -0,0 +1,8 @@
+// export const size = {
+//   tablet: "768px",
+// };
+const device = {
+  tablet: `(min-width: 768px)`,
+};
+
+export default device;
